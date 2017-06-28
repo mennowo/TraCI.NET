2e640e416186c3544b7fd6eb4dab6b62f0821844
menno doeterniettoe <mennowo@gmail.com>
2017-06-28 17:50:14 +0200

    Add project files.

---
+++ TraCI.NET/TraCICommand.cs
@@ -0,0 +1,8 @@
+﻿namespace TraCI.NET
+{
+    internal class TraCICommand
+    {
+        public byte Identifier { get; set; }
+        public byte[] Contents { get; set; }
+    }
+}
---
+++ TraCI.NET/TraCIResult.cs
@@ -0,0 +1,9 @@
+﻿namespace TraCI.NET
+{
+    internal class TraCIResult
+    {
+        public int Length { get; set; }
+        public byte Identifier { get; set; }
+        public byte[] Response { get; set; }
+    }
+}
